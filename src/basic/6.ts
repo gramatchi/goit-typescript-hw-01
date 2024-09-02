c4ae396dc5e613fe72e66fc8d7806c66d1c90beb
gramatchi <nik98.champion@gmail.com>
2024-09-02 18:22:42 +0200

    7.ts

--- src/basic/6.ts
+++ src/basic/6.ts
@@ -1,25 +1,25 @@
 interface User {
-    name: string,
-    age: number,
-    email: string,
-    address?: {
-        city: string,
-        country: string,
-    }
+  name: string;
+  age: number;
+  email: string;
+  address?: {
+    city: string;
+    country: string;
+  };
 }
 
-const mango : User= {
-    name: 'Mango',
-    age: 30,
-    email: 'john@example.com',
-    address: {
-        city: 'New York',
-        country: 'USA'
-    }
+const mango: User = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+  address: {
+    city: "New York",
+    country: "USA",
+  },
 };
 
-const poly : User= {
-    name: 'Mango',
-    age: 30,
-    email: 'john@example.com'
+const poly: User = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
 };
